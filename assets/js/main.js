644338498ac91eb636ed93235db7b25233d37089
sweet202020 <dolcealessio1@gmail.com>
2022-10-12 17:08:55 +0200

    fix carousel item

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,7 @@ const immaginiCarosello = [
     "./assets/img/05.webp", 
 ]
 //seleziono la parte in cui voglio metere le mie immagini nella dom
-const carouselInner = document.querySelector('.slider>.carousel-inner')
+const carouselInner = document.querySelector('.slider>.carousel-inner');
 
 //utilizzo il ciclo for pr stampare le immagini
 let activeImage= immaginiCarosello [0]
@@ -18,11 +18,16 @@ for (i=0; i < immaginiCarosello.length; i++ ){
     const immagine = immaginiCarosello [i];
     console.log(immagine);
     //inserisco le varie immagini nel carousel-inner
-    const carouselItem = `<div class="carousel-item"><img src=${immagine}></div>`;
-    console.log(carouselItem)
-    carouselInner.insertAdjacentHTML("beforeend", carouselItem)
+    const carouselItem = `<div class="${i===0 ? 'active' : ''}carousel-item"><img src=${immagine}></div>`;
+    console.log(carouselItem);
+    carouselInner.insertAdjacentHTML("beforeend", carouselItem);
+
 }
 
 //aggiungo l'active nella prima immagine
-activeImage = document.querySelector(".carousel-item").classList.add('active')
+const buttonNext = document.querySelector('.next')
+buttonNext.addEventListener("click", function(){
+    
 
+
+})
