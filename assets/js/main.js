1c39b45e48077095b33167fa5a0b10da0905b5bf
sweet202020 <dolcealessio1@gmail.com>
2022-10-12 15:46:49 +0200

    milestone-1

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+//Dato un array contenente una lista di cinque immagini, creare un carosello come nello screenshot allegato.
+
+//creo l'array con la lista delle immagini
+const carosello = [
+    "./assets/img/01.webp",
+    "./assets/img/02.webp",
+    "./assets/img/03.webp",
+    "./assets/img/04.webp",
+    "./assets/img/05.webp", 
+]
