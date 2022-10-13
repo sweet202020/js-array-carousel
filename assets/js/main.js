fc987473b45c04d8ca35883001939beba724cca7
sweet202020 <dolcealessio1@gmail.com>
2022-10-13 19:42:38 +0200

    final commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,43 +1,54 @@
-//Dato un array contenente una lista di cinque immagini, creare un carosello come nello screenshot allegato.
-
-//creo l'array con la lista delle immagini
-const immaginiCarosello = [
-    "./assets/img/01.webp",
-    "./assets/img/02.webp",
-    "./assets/img/03.webp",
-    "./assets/img/04.webp",
-    "./assets/img/05.webp", 
+//creo un array contenente le mie 5 immagini
+const images = [
+    './assets/img/01.webp',
+    './assets/img/02.webp',
+    './assets/img/03.webp',
+    './assets/img/04.webp',
+    './assets/img/05.webp',
 ]
-//seleziono la parte in cui voglio metere le mie immagini nella dom
-const carouselInner = document.querySelector('.slider>.carousel-inner');
 
-//utilizzo il ciclo for pr stampare le immagini
-let activeImage= 0
-console.log(activeImage); 
-for (i=0; i < immaginiCarosello.length; i++ ){
-    const immagine = immaginiCarosello [i];
-    console.log(immagine);
-    //inserisco le varie immagini nel carousel-inner
-    //aggiungo la classe active in posizione 0
-    const carouselItem = `<div class="${i===activeImage ? 'active' : ''} carousel-item"><img src=${immagine}></div>`;
-    console.log(carouselItem);
-    carouselInner.insertAdjacentHTML("beforeend", carouselItem);
-
+//seleziono l'elemento della dom dove voglio inserire le mie immagini
+const sliderElements= document.querySelector('.slider');
+/* console.log(sliderElements) */
+//ciclo for per la lista delle immagini
+let activeImage=0
+for (let i=0; i<images.length; i++){
+    image = images[i];
+    /* console.log(image); */
+    const imgElement = `<img class="${i===activeImage ? 'active' : ''}" src=${image}>`
+    sliderElements.insertAdjacentHTML('beforeend', imgElement)
 }
 
-//funzione per il click del next
-const buttonNext = document.querySelector('.next')
-buttonNext.addEventListener("click", function(){
-    console.log('next')
-    const currentActive = document.querySelector("div.active")
-    console.log(currentActive)
-
-    currentActive.classList.remove('active')
-
+//seleziono il bottone dalla dom
+const buttonGeneratorNext = document.querySelector ('.next')
+/* console.log (buttonGenerator) */
+buttonGeneratorNext.addEventListener('click', function(){
+    //selezionare l'elemento con la classe active
+    const allImages = document.querySelectorAll('.slider > img')
+    /* console.log(allImages) */
+    /* console.log( allImages[activeImage]) */
+    //rimuovere la classe active
+    allImages[activeImage].classList.remove('active')
+    //incremento activeImage di uno
     activeImage++
+    //aggiunco la classe active
+    allImages[activeImage].classList.add('active')
+})
 
-    const nextImg = document.querySelector()
-    console.log(nextImg)
-    nextImg.classList.add('active')
 
+
+//seleziono il bottone dalla dom
+const buttonGeneratorPrev = document.querySelector ('.prev')
+/* console.log (buttonGenerator) */
+buttonGeneratorPrev.addEventListener('click', function(){
+    //selezionare l'elemento con la classe active
+    const allImages = document.querySelectorAll('.slider > img')
+    /* console.log(allImages) */
+    /* console.log( allImages[activeImage]) */
+    //rimuovere la classe active
+    allImages[activeImage].classList.remove('active')
+    //incremento activeImage di uno
+    activeImage--
+    //aggiunco la classe active
+    allImages[activeImage].classList.add('active')
 })
